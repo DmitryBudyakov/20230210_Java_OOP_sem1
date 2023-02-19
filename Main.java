957b22c32fcc3b545070da52cf68fce5b1b53936
Dmitry Budyakov <dnikbu@yandex.ru>
2023-02-19 21:31:36 +0300

    Созданные классы

---
+++ Main.java
@@ -0,0 +1,23 @@
+import units.Countryman;
+import units.Magicman;
+import units.Monk;
+import units.Robber;
+import units.Sniper;
+import units.Spearman;
+import units.XBowman;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Countryman cm1 = new Countryman("Никола");
+        Magicman mag1 = new Magicman("Гуатамо");
+        Monk monk1 = new Monk("Пористус");
+        Robber rob1 = new Robber("Ловкач");
+        Sniper sni1 = new Sniper("Меткач");
+        Spearman spe1 = new Spearman("Остряк");
+        XBowman xbow1 = new XBowman("Робин");
+    }
+}
---
+++ units/XBowman.java
@@ -0,0 +1,31 @@
+// Арбалетчик
+package units;
+
+public class XBowman extends Man {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public XBowman(String name) {
+        super(100, 3, 25, 25, 5);
+        this.name = name;
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        String output = String.format("Арбалетчик %s: hp = %d, maxHp = %d, defence = %d, attack = %d, speed = %d",
+                                        name, hp, maxHp, defence, attack, speed);
+        return output;
+    }
+
+
+    
+}
